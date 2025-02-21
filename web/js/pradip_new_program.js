2bb124bd716964ec41910b35ea5b9f77d6c3dce6
juniorborelli <aborelli@murrayphn.org.au>
2025-02-21 15:24:00 +1100

    new update

--- web/js/pradip_new_program.js
+++ web/js/pradip_new_program.js
@@ -3,13 +3,14 @@ let $x = 10;
 alert($x);
 
 resetButton = document.getElementById("resetButton");
+
 resetButton.addEventListener("click", function () {
     alert("Reset button clicked!");
 });
 
 
 document.getElementById("myButton").addEventListener("click", function () {
-  alert("Button clicked!");
+  
   let $x = 20;
   alert(Today());
 });
